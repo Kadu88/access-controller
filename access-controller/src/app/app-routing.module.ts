0e5659783e3881c6437a5968f9ae8fdb5a89a8a7
Carlos Barros <carlosuntr@gmail.com>
2019-02-10 21:21:43 -0200

    projeto com roteamento ok e login form montado

--- access-controller/src/app/app-routing.module.ts
+++ access-controller/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SigninComponent } from './home/signin/signin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MonitorComponent } from './dashboard/monitor/monitor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: SigninComponent
+    },
+    {
+        path: 'monitor',
+        component: MonitorComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ access-controller/src/app/photos/photos.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { CardModule } from '../shared/components/card/card.module';
+import { DarkenOnHoverModule } from '../shared/directives/darken-on-hover/darken-on-hover.module';
+
+@NgModule({
+    imports: [
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+        CardModule,
+        DarkenOnHoverModule
+    ]
+})
+export class PhotosModule { }
